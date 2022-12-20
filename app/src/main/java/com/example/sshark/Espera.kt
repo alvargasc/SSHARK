a29c5227ee00c6660f2d2b68dd6b44c7e1ebf85e
Fcruzado <fcruzado@soaint.com>
2022-12-20 00:26:41 -0500

    create espera

---
+++ app/src/main/java/com/example/sshark/Espera.kt
@@ -0,0 +1,11 @@
+package com.example.sshark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Espera : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_espera)
+    }
+}
